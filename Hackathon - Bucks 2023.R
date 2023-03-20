936ebc24051b491a674260239538ce401e727ccb
robo515 <128259286+robo515@users.noreply.github.com>
2023-03-20 11:33:40 -0500

    Add files via upload

--- Hackathon - Bucks 2023.R
+++ Hackathon - Bucks 2023.R
@@ -20,6 +20,31 @@ table(Nading$offBall_actions)
 #259 off_ball actions
 259/1596
 
+#heatmap
+df_subset <- Nading[, c("iso_actions", "post_actions", "pick_actions", "offBall_actions")]
+subset <- data[, c("iso_actions", "post_actions", "pick_actions", "offBall_actions")]
+sand_subset <- Sandoval[, c("iso_actions", "post_actions", "pick_actions", "offBall_actions")]
+# identify missing values (NA)
+df_subset[is.na(df_subset)] <- "Missing"
+subset[is.na(subset)] <- "Missing"
+sand_subset[is.na(sand_subset)] <- "Missing"
+# calculate the frequency of each column containing a number
+freq <- colSums(df_subset != "Missing")
+freq <- freq/sum(freq)
+freq2 <- colSums(subset != "Missing")
+freq2 <- freq2/sum(freq2)
+freq3 <- colSums(sand_subset != "Missing")
+freq3 <- freq3/sum(freq3)
+# create a matrix with at least 2 rows and 2 columns
+barplot(freq, freq2, beside = TRUE)
+
+
+f <- t(cbind(freq, freq2))
+barplot(f, beside=TRUE,  col=c("blue", "red"), 
+        legend=c("Nading", "All Players"),
+        names.arg=c("iso_actions", "post_actions", "pick_actions", "offBall_actions"))
+
+
 273 + 944 +120+259
 str(Nading)
 mean(Nading$player_off_ht)
@@ -45,6 +70,9 @@ mean(Nading$qSQ)
 #so we can use column means
 new_Nading <- Nading[c(-1,-2)]
 
+
+sum(Nading$fg3, na.rm = TRUE)/sum(Nading$fga3, na.rm = TRUE)
+sum(data$fg3, na.rm = TRUE)/sum(data$fga3, na.rm = TRUE)
 #Nading_Iso
 Nading_Iso <- new_Nading %>% drop_na(iso_actions)
 iso_avg_points <- mean(Nading_Iso$iso_pts)
@@ -147,14 +175,68 @@ data_Def_Post <- aggregate(post_pts ~ player_defMatchup_a, data = data, FUN = me
 data_Def_Off <- aggregate(offBall_pts ~ player_defMatchup_a, data = data, FUN = mean, na.rm = TRUE, drop = FALSE)
 
 data_Def_qSQ <- aggregate(qSQ ~ player_defMatchup_a, data = data, FUN = mean, na.rm = TRUE, drop = FALSE)
-#Dataframe of all avg stats for defenders against Nading
+#Dataframe of all avg stats for defenders against Nading's tendencies
 Total_Def_Stats <- merge(Total_Def_Stats, data_Def_Off,by = 'player_defMatchup_a')
 
 Total_Def_Stats$Rank <- (Total_Def_Stats$iso_pts * .171) + (Total_Def_Stats$pick_pts * .591) + (Total_Def_Stats$post_pts * .075) + (Total_Def_Stats$offBall_pts * .162)
 
-Total_Def_Stats$na_Rank <- ((ifelse(is.na(Total_Def_Stats$iso_pts), 0, Total_Def_Stats$iso_pts) * .171) + 
-                                     (ifelse(is.na(Total_Def_Stats$pick_pts), 0, Total_Def_Stats$pick_pts) * .591) + 
-                                     (ifelse(is.na(Total_Def_Stats$post_pts), 0, Total_Def_Stats$post_pts) * .075) + 
-                                     (ifelse(is.na(Total_Def_Stats$offBall_pts), 0, Total_Def_Stats$offBall_pts) * .162)) 
-
 Total_Def_Stats <- Total_Def_Stats[order(Total_Def_Stats$Rank),]
+
+
+
+#Seeing the ranks for Sandoval
+
+Sandoval_Tendencies <- Total_Def_Stats
+Sandoval_Tendencies$Rank <- (Sandoval_Tendencies$iso_pts * .157) + (Sandoval_Tendencies$pick_pts * .530) + (Sandoval_Tendencies$post_pts * .003) + (Sandoval_Tendencies$offBall_pts * .309)
+Sandoval_Tendencies <- Sandoval_Tendencies[order(Sandoval_Tendencies$Rank),]
+
+
+
+
+
+#Looking at Joshua Sandoval - most similar player to Nading
+Sandoval <- data[data$player_off_a == 'Sandoval, Joshua',]
+
+Sandoval$player_defMatchup_a <- factor(Sandoval$player_defMatchup_a, levels = unique(Sandoval$player_defMatchup_a))
+Sandoval_Def_Iso <- aggregate(iso_pts ~ player_defMatchup_a, data = Sandoval, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+Sandoval_Def_Pick <- aggregate(pick_pts ~ player_defMatchup_a, data = Sandoval, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+Sandoval_Def_Post <- aggregate(post_pts ~ player_defMatchup_a, data = Sandoval, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+Sandoval_Def_Off <- aggregate(offBall_pts ~ player_defMatchup_a, data = Sandoval, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+Sandoval_Def_qSQ <- aggregate(qSQ ~ player_defMatchup_a, data = Sandoval, FUN = mean, na.rm = TRUE, drop = FALSE)
+#Dataframe of all avg stats for defenders against Nading
+Total_Sandoval_Def_Stats <- merge(Sandoval_Def_Iso, Sandoval_Def_Pick, by = 'player_defMatchup_a')
+Total_Sandoval_Def_Stats <- merge(Total_Sandoval_Def_Stats, Sandoval_Def_Off, by = 'player_defMatchup_a')
+
+Total_Sandoval_Def_Stats$Rank <- (Total_Sandoval_Def_Stats$iso_pts * .171) + (Total_Sandoval_Def_Stats$pick_pts * .591) + (Total_Sandoval_Def_Stats$post_pts * .075) + (Total_Sandoval_Def_Stats$offBall_pts * .162)
+
+Total_Sandoval_Def_Stats$na_Rank <- ((ifelse(is.na(Total_Sandoval_Def_Stats$iso_pts), 0, Total_Sandoval_Def_Stats$iso_pts) * .171) + 
+                                     (ifelse(is.na(Total_Sandoval_Def_Stats$pick_pts), 0, Total_Sandoval_Def_Stats$pick_pts) * .591) + 
+                                     (ifelse(is.na(Total_Sandoval_Def_Stats$post_pts), 0, Total_Sandoval_Def_Stats$post_pts) * .075) + 
+                                     (ifelse(is.na(Total_Sandoval_Def_Stats$offBall_pts), 0, Total_Sandoval_Def_Stats$offBall_pts) * .162)) 
+
+Total_Sandoval_Def_Stats <- Total_Sandoval_Def_Stats[order(Total_Sandoval_Def_Stats$Rank),]
+
+
+Similar <- data[data$player_off_a == 'Sandoval, Joshua' | data$player_off_a == 'Oscarson, Jeremy' | data$player_off_a == 'Hammond, Brandyn' | data$player_off_a == 'Thin-Elk, Edward' | data$player_off_a == 'Kierstead, Iaasic',]
+#Similar 5 players and their ranks 
+Similar$player_defMatchup_a <- factor(Similar$player_defMatchup_a, levels = unique(Similar$player_defMatchup_a))
+Similar_Def_Iso <- aggregate(iso_pts ~ player_defMatchup_a, data = Similar, FUN = mean, na.rm = TRUE, drop = FALSE)
+Similar_Def_Iso <- Nading_Def_Iso[order(Nading_Def_Iso$iso_pts),]
+
+Similar_Def_Pick <- aggregate(pick_pts ~ player_defMatchup_a, data = Similar, FUN = mean, na.rm = TRUE, drop = FALSE)
+Similar_Def_Pick <- Nading_Def_Pick[order(Nading_Def_Pick$pick_pts),]
+
+Similar_Def_Post <- aggregate(post_pts ~ player_defMatchup_a, data = Similar, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+Similar_Def_Off <- aggregate(offBall_pts ~ player_defMatchup_a, data = Similar, FUN = mean, na.rm = TRUE, drop = FALSE)
+
+
+Total_Similar_Def_Stats <- merge(Total_Similar_Def_Stats, Similar_Def_Off, by = 'player_defMatchup_a')
+
+Total_Similar_Def_Stats$Rank <- mean(Total_Similar_Def_Stats$iso_pts * Total_Similar_Def_Stats$pick_pts * Total_Similar_Def_Stats$post_pts * Total_Similar_Def_Stats$offBall_pts, na.rm = TRUE)
+
+
